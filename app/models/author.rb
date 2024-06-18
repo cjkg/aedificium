2ec5ed10375cffc1621b49f3f88b591268df104c
Christopher Gibilisco <cjgibilisco@gmail.com>
2024-06-17 21:26:03 -0500

    ugly book page

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,9 @@ class Author < ApplicationRecord
   has_many :books
 
   validates :first_name, presence: true
+
+  def author_first_last
+    last_name = self.last_name.present? ? " " + self.last_name : ""
+    self.first_name + last_name
+  end
 end
