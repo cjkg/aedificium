ba93880d9b619b15a1a6422052d4f643d6d88686
Christopher Gibilisco <cjgibilisco@gmail.com>
2024-06-23 22:03:38 -0500

    transformer rework

--- app/ingress/open_library_books_transformer.rb
+++ app/ingress/open_library_books_transformer.rb
@@ -7,72 +7,61 @@ class OpenLibraryBooksTransformer < BaseTransformer
     @api = OpenLibraryBooksApi.new
   end
 
-  def transform_isbn(isbn, room_id)
+  def transform_isbn(isbn)
     isbn = isbn_cleaner(isbn)
 
     book_key = @api.isbn(isbn).parsed_response["key"]
 
     if book_key.nil?
       pp "Book not found in Open Library"
-      return
+      return {},[{}]
     end
 
     book_info = @api.basic_call(book_key).parsed_response
 
-    title = book_info["title"]
+    book = {}
 
-    isbn_10 = book_info["isbn_10"]&.first
-    isbn_13 = book_info["isbn_13"]&.first || source_to_isbn_13(book_info.dig("source_records")) # TODO absolutely has to be a better way to do this
+    book[:title] = book_info["title"]
 
-    book_candidates = Book.where(title: title, isbn_10: isbn).or(Book.where(title: title, isbn_13: isbn))
+    book[:isbn_10] = book_info["isbn_10"]&.first || ""
+    book[:isbn_13] = book_info["isbn_13"]&.first || source_to_isbn_13(book_info.dig("source_records")) || "" # TODO absolutely has to be a better way to do this
 
-    copy = book_candidates.length + 1
+    book_candidates = Book.where(title: book[:title], isbn_10: isbn).or(Book.where(title: book[:title], isbn_13: isbn))
 
-    book = Book.new(title: title, isbn_10: isbn_10, isbn_13: isbn_13, copy: copy, room_id: room_id)
+    book[:copy] = book_candidates.length + 1
 
     # Edition data
 
-    subtitle = book_info["subtitle"]
-    book.update(subtitle: subtitle) if subtitle.present?
+    book[:subtitle] = book_info["subtitle"] || ""
 
-    if book_info["languages"].nil?
-      pp "Language not found in Open Library"
-    else
+    language = ""
+
+    if book_info["languages"].present?
       language_key = book_info["languages"]&.first["key"]
       language_response = @api.basic_call(language_key).parsed_response
       language = language_response["name"]
-      book.update(language: language) if language_response.present?
     end
 
-    publishers = array_to_string_list(book_info["publishers"])
-    book.update(publisher: publishers) if publishers.present?
+    book[:language] = language
 
-    published = book_info["publish_date"]
-    book.update(published: published) if published.present?
+    book[:publishers] = array_to_string_list(book_info["publishers"]) || ""
+    book[:published] = book_info["publish_date"] || ""
+    book[:location] = book_info["publish_places"]&.first || ""
 
-    location = book_info["publish_places"]&.first
-    book.update(location: location) if location.present?
+    book[:format] = book_info["physical_format"] || ""
+    book[:edition] = book_info["edition_name"] || ""
 
-    format = book_info["physical_format"]
-    book.update(format: format) if format.present?
-
-    edition = book_info["edition_name"]
-    book.update(edition: edition) if edition.present?
-
-    if book_info["number_of_pages"]&.to_i > 0
-      book.update(pages: book_info["number_of_pages"])
+    if book_info["number_of_pages"].present?
+      book[:pages] = book_info["number_of_pages"]&.to_i > 0 ? book_info["number_of_pages"].to_s : book[:pages] = ""
+    else
+      book[:pages] = ""
     end
 
     goodreads_id = book_info.dig("identifiers", "goodreads")&.first
-    book.update(goodreads_id: goodreads_id) if goodreads_id.present?
+    book[:goodreads_url] = goodreads_id.present? ? "https://www.goodreads.com/book/show/#{goodreads_id}" : ""
 
     librarything_id = book_info.dig("identifiers", "librarything")&.first
-    book.update(librarything_id: librarything_id) if librarything_id.present?
-
-    unless book.save
-      pp book.errors.messages
-      return
-    end
+    book[:librarything_url] =  librarything_id.present? ? "https://www.librarything.com/work/#{librarything_id}" : ""
 
     # Work data
 
@@ -82,42 +71,27 @@ class OpenLibraryBooksTransformer < BaseTransformer
 
     author_keys = works_info["authors"]
 
+    authors = []
+
     author_keys.each do |ak|
       current_key_string = ak["author"]["key"]
       author_response = @api.basic_call(current_key_string).parsed_response
 
-      name = author_response["name"] || author_response["personal_name"]
-      dob = author_response["birth_date"]
-
-      author = Author.find_or_create_by(name: name, date_of_birth: dob)
-
-      dod = author_response["death_date"]
-      author.update(date_of_death: dod) if author.date_of_death.nil?
-
-      bio = author_response["bio"] || ""
+      author = {}
+      author[:name] = author_response["name"] || author_response["personal_name"] || ""
+      author[:dob] = author_response["birth_date"] || ""
+      author[:dod] = author_response["death_date"] || ""
 
       # TODO get this working:
       begin
-        bio = eval(bio)["value"] # Sometimes it returns a stringified ruby object (!?!) instead of a string?
+        bio = eval(bio)["value"] || "" # Sometimes it returns JSON, sometimes just a string
       rescue NameError, TypeError
-        pp "bio not a ruby object -- using raw bio instead"
+        bio = author_response["bio"] || ""
       end
 
-      author.update(bio: bio) if author.bio.nil?
-
-      unless author.save
-        pp author.errors.messages
-        return
-      end
-
-      book_author_pair = BookAuthor.find_or_initialize_by(book_id: book.id, author_id: author.id)
-
-      unless book_author_pair.save
-        pp book_author_pair.errors.messages
-        return
-      end
+      authors << author
     end
-
+    return book, authors
   end
 
   private
